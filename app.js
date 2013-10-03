f7dba260c5302f9a72814ebb68df084441e09ca0
up2frets <dexter.crossfire@gmail.com>
2013-10-03 23:00:01 +0300

    New files

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ app.get('/ex', routes.ex);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port') + process.version);
 });
