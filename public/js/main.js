f0e52034fb31f5d366c0db7fbd20b5e2c44be311
up2frets <dexter.crossfire@gmail.com>
2014-03-11 23:17:49 +0200

    Major

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,67 @@
-var app = angular.module('up2frets', ['pascalprecht.translate']);
+$(function() {
 
-app.config(function ($translateProvider) {
-  $translateProvider.translations('en', {
-    TITLE: 'Hello',
-    FOO: 'This is a paragraph.',
-    BUTTON_LANG_EN: 'english',
-    BUTTON_LANG_DE: 'german'
-  });
-  $translateProvider.translations('de', {
-    TITLE: 'Hallo',
-    FOO: 'Dies ist ein Paragraph.',
-    BUTTON_LANG_EN: 'englisch',
-    BUTTON_LANG_DE: 'deutsch'
-  });
-  $translateProvider.preferredLanguage('en');
-});
+	var $mpHome = $('.mp-home'),
+		$header = $('header'),
+		$headerLink = $header.find('.header_nav a'),
+		$section = $('.mp'),
+		$footer = $('footer'),
+		$btnScrollBottom = $('.mp-projects_scroll a')
 
-app.controller('Ctrl', function ($scope, $translate) {
-  $scope.changeLanguage = function (key) {
-    $translate.uses(key);
-  };
-});
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    console.log($(target));
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+	var body = document.body,
+    timer;
+
+	window.addEventListener('scroll', function() {
+	  clearTimeout(timer);
+	  if(!body.classList.contains('disable-hover')) {
+	    body.classList.add('disable-hover')
+	  }
+	  
+	  timer = setTimeout(function(){
+	    body.classList.remove('disable-hover')
+	  },200);
+	}, false);
+
+	$(window).on('load scroll resize', function(){
+		scrollSpy();
+	});
+
+	function scrollSpy(){
+		var currentElement = $('.mp:in-viewport').attr('id'),
+			patternHref = '[href $= "{currentElement}"]'.replace('{currentElement}', currentElement),
+			patternId = '[id $= "{currentElement}"]'.replace('{currentElement}', currentElement),
+			footerBottom = $(document).height() - $(window).scrollTop() - $(window).height(); 
+
+		$(window).scrollTop() >= $mpHome.height() ? $header.addClass('ready') : $header.removeClass('ready');
+		footerBottom <= 360 ? $btnScrollBottom.addClass('active') : $btnScrollBottom.removeClass('active')
+
+		$headerLink.removeClass('active');
+		//$section.removeClass('active');
+
+
+		if (footerBottom == 0) {
+			$headerLink.filter('[href $= "contacts"]').addClass('active');
+		} else {
+			$headerLink.filter(patternHref).addClass('active');
+			$section.filter(patternId).addClass('active');
+
+			console.log(patternId);
+		}
+
+	}
+
+})
